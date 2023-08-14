ba09bb1930309b430b2fa7ed38759779e91cde1c
Jose Toscano <josetoscano@atratopago.com>
2023-08-14 15:11:23 -0600

    app setup is ready

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import "reflect-metadata"
 import "dotenv/config"
-import { AppDataSource } from "./typeorm/data-source";
+import AppDataSource from "./typeorm/data-source";
+import app from './server'
+
+const port = process.env.PORT || 3000;
 
 
 AppDataSource.initialize().then(async () => {
     console.log("Database initialized successfully");
     // Start server here
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 }).catch(error => console.log(error))
